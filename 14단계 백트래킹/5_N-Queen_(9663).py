14cb73e52a00cd1884797538e16d55572a5909ae
limdonggeon-20211485 <dlaehdrjs147@naver.com>
2022-03-14 02:45:01 +0900

    7 commit

--- 14단계 백트래킹/5_N-Queen_(9663).py
+++ 14단계 백트래킹/5_N-Queen_(9663).py
@@ -1,37 +1,4 @@
 # 나중에 다시 풀어보기
-'''
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10000) # 재귀 함수 깊이의 한도를 10000까지 늘려준다 (원래 1000)
-
-N = int(input())
-
-answer = 0
-board = [-1] * N  # 1차원 배열로도 가능 board[i] = j 의 의미는 (i, j)
-
-def is_promising(x):
-    for i in range(x):
-        if board[x] == board[i] or abs(board[x] - board[i]) == abs(x - i): # 같은 열에 퀸이 있거나, 대각선 상에 있으면 거짓
-            return False
-    
-    return True
-
-def n_queens(x):
-    global answer      # 함수 안에서 answer을 사용하겠다
-    if x == N:
-        answer += 1
-
-    else:
-        for i in range(N):
-            # [x, i]에 퀸을 놓겠다.
-            board[x] = i
-            if is_promising(x):   # 만약 다음줄에 놓을 수 있으면 다음 줄 호출
-                n_queens(x+1)
-
-n_queens(0)
-print(answer)
-'''
-
 import sys
 input = sys.stdin.readline
 
